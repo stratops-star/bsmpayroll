9ece2d74ea404668eda6d9b132d583ce3ae4206d
stratops-star <strat.ops@bsmfacilitysolutions.com>
2026-07-05 09:41:08 -0400

    Update layout.tsx

--- src/app/recruiting/layout.tsx
+++ src/app/recruiting/layout.tsx
@@ -1,5 +1,11 @@
 import AccessGate from '@/components/AccessGate'
+import NavBar from '@/components/NavBar'
 
 export default function RecruitingLayout({ children }: { children: React.ReactNode }) {
-  return <AccessGate requireDepartment="recruiting">{children}</AccessGate>
+  return (
+    <AccessGate requireDepartment="recruiting">
+      <NavBar />
+      {children}
+    </AccessGate>
+  )
 }
