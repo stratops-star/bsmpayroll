b36c1268f11ed34bdc197446ea59d955419423e9
stratops-star <strat.ops@bsmfacilitysolutions.com>
2026-07-05 13:14:20 -0400

    Update layout.tsx

--- src/app/recruiting/layout.tsx
+++ src/app/recruiting/layout.tsx
@@ -1,14 +1,17 @@
 import AccessGate from '@/components/AccessGate'
 import NavBar from '@/components/NavBar'
 import { RecruitingChromeProvider } from '@/components/RecruitingChrome'
+import { RecruitingLangProvider } from '@/components/recruiting-i18n'
 
 export default function RecruitingLayout({ children }: { children: React.ReactNode }) {
   return (
     <AccessGate requireDepartment="recruiting">
-      <RecruitingChromeProvider>
-        <NavBar />
-        {children}
-      </RecruitingChromeProvider>
+      <RecruitingLangProvider>
+        <RecruitingChromeProvider>
+          <NavBar />
+          {children}
+        </RecruitingChromeProvider>
+      </RecruitingLangProvider>
     </AccessGate>
   )
 }
